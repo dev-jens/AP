5365a499a667341701257551a5ff88635fe68db0
Dev-jens <baetenjens07@gmail.com>
2021-03-25 14:25:14 +0100

    update 25/0

---
+++ 2020-2021/semester2/Data Structures/labo2/stack/stack reverse/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stack_reverse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReverseKnop_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputfield.Text;
+            string[] inputSplit = input.Split(",");
+
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+                int value = int.Parse(inputSplit[i]);
+                push(value);
+            }
+
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+                int value = pop();
+                outputField.Text += $"{value.ToString()}, ";
+            }
+            
+        }
+
+        int[] stack = new int[5];
+        int top = -1;
+
+        private void push(int input)
+        {
+            if (top == stack.Length - 1) { return; }
+            top++;
+            stack[top] = input;
+        }
+
+        private int pop()
+        {
+            int input = stack[top];
+            top--;
+            return input;
+        }
+    }
+}
---
+++ 2020-2021/semester2/Data Structures/labo2/stack/stackv1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stackV1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pushKnop_Click(object sender, RoutedEventArgs e)
+        {
+            int input = int.Parse(inputStack.Text);
+            push(input);
+
+            inputStack.Text = "";
+
+            // Enable Pop button is it's disabled
+            if (!popKnop.IsEnabled)
+                popKnop.IsEnabled = true;
+        }
+
+        private void popKnop_Click(object sender, RoutedEventArgs e)
+        {
+            int waarde = pop();
+
+            outputStack.Items.Add(waarde.ToString());
+
+       
+        }
+
+        int[] stack = new int[5];
+        int top = -1;
+
+
+        private void push(int input)
+        {
+            if (top == stack.Length-1){ return; }
+            top++;
+            stack[top] = input;
+            
+        }
+
+        private int pop() 
+        {
+            int input = stack[top];
+            top--;  
+            return input;
+        
+        }
+
+
+    }
+}
